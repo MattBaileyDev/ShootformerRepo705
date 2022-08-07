34129ee7bc4eb0329b1763051eb086be4ff2e971
Matthew Bailey <mb214650@falmouth.ac.uk>
2022-08-07 21:30:47 +0100

    Fixing Bugs in Level 2 and Begun Work on Checkpoints

---
+++ Shootformer/Assets/Scripts/Checkpoints.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Checkpoints : MonoBehaviour
+{
+    public DeathBox db;
+    public GameObject Checkpoint1;
+    public GameObject Checkpoint2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (GetComponent<Collider>().name == "Checkpoint 1")
+        {
+            db.reachedCheckpoint1 = true;
+
+        }
+        else
+        if (GetComponent<Collider>().name == "Checkpoint 2")
+        {
+            db.reachedCheckpoint1 = true;
+        }
+    }
+}
--- Shootformer/Assets/Scripts/DeathBox.cs
+++ Shootformer/Assets/Scripts/DeathBox.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.Debug;
+
+
 public class DeathBox : MonoBehaviour
 {
+    public bool reachedCheckpoint1 = false;
+    public bool reachedCheckpoint2 = false;
+    public Transform Player;
+    public Transform Checkpoint1;
+    public Transform Checkpoint2;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +28,22 @@ public class DeathBox : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (reachedCheckpoint1 == false && other.gameObject.tag == "Player")
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        if (reachedCheckpoint1 == true && other.gameObject.tag == "Player")
+        {
+            Player.transform.position = Checkpoint1.position;
+            
+        }
+        else
+        if(reachedCheckpoint2 == true && other.gameObject.tag == "Player")
+        {
+            Player.transform.position = Checkpoint2.position;
+           
+
         }
     }
 }
